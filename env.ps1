bec84005ba08c15d0f19c7a6a11c6a4fb447ae6d
a-b365 <alphaelite10@gmail.com>
2025-05-26 23:14:44 +0545

    start

---
+++ env.ps1
@@ -0,0 +1,7 @@
+# Set environment variables for the current session
+$env:ORIGINAL_DATA_PATH = "D:\classifier-algorithms\datasets\TASK_2\TASK_2"
+$env:MODEL_PATH = "D:\classifier-algorithms\models\"
+
+Write-Host "Environment variables set:"
+Write-Host "ORIGINAL_DATA_PATH=$env:ORIGINAL_DATA_PATH"
+Write-Host "MODEL_PATH=$env:MODEL_PATH"
