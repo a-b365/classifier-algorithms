c7f4a0e836cbf2edf2e5ac120e8d14423178dc87
a-b365 <alphaelite10@gmail.com>
2025-05-27 18:01:59 +0545

    pipelines

---
+++ utils/random_forest.py
@@ -0,0 +1,44 @@
+# Standard library imports
+import os
+
+# Third-party imports 
+import numpy as np
+import pandas as pd
+
+# Relative imports
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import StandardScaler, RobustScaler
+from sklearn.feature_selection import SelectKBest, f_classif, VarianceThreshold
+from sklearn.impute import SimpleImputer, KNNImputer
+
+
+if __name__ == "__main__":
+    
+    pipeline = Pipeline([
+        ("imputer", SimpleImputer(missing_values=np.nan, strategy="median")),
+        ("scalar", StandardScaler()),
+        # ("selector_i", VarianceThreshold(threshold=0.01)),
+        # ("selector_ii", SelectKBest(score_func=f_classif, k=100)),
+        ("model", RandomForestClassifier(max_depth=None, max_features="log2", n_estimators=100, min_samples_leaf=1, min_samples_split=20))
+    ])
+
+    TRAIN_DATA = os.environ["ORIGINAL_DATA_PATH"] + "/train_set.csv"
+    TEST_DATA = os.environ["ORIGINAL_DATA_PATH"] + "/test_set.csv"
+    
+    train_data = pd.read_csv(TRAIN_DATA)
+    test_data = pd.read_csv(TEST_DATA)
+
+    X_train = train_data.drop(columns=["ID", "CLASS"])
+    y_train = train_data["CLASS"]
+    X_test = test_data.drop(columns=["ID", "CLASS"])
+    y_test = test_data["CLASS"]
+
+    X_clean = X_train.replace([np.inf, -np.inf], np.nan)
+
+    print(pipeline.fit(X_clean, y_train).score(X_test, y_test))
+    # mask = pipeline.fit(X_clean, y).named_steps["selector_i"].get_support(indices=True)
+    # selected_features = X_clean.columns[mask]
+    # print(f"Reduced from {X_clean.shape[1]} to {len(mask)} features.")
+    
+
